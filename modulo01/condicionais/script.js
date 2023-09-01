a36d909fe5b57c21727c09bd491243674efaed72
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-08-31 22:33:10 -0300

    Estudando sobre variáveis

--- modulo01/condicionais/script.js
+++ modulo01/condicionais/script.js
@@ -1,4 +1,4 @@
-var hora = 8;
+var hora = 12;
 
 if (hora < 12) {
     console.log("Bom dia");
@@ -6,4 +6,12 @@ if (hora < 12) {
     console.log("Boa tarde")
 } else {
     console.log("Boa noite")
+}
+
+if (hora >= 12 && hora < 18) {
+    console.log("BO BO BO BO BOA TARDE")
+}
+
+if (hora == 12 || hora == 18) {
+    console.log("HORA DO RUSH")
 }
--- modulo01/variaveis/script.js
+++ modulo01/variaveis/script.js
@@ -1,3 +1,5 @@
+/*
+
 var x = 10;
 var y = 15;
 
@@ -9,4 +11,27 @@ var sobrenome = "Santos Pereira";
 
 var nome_completo = nome + " " + sobrenome;
 
-window.alert(nome_completo);
+window.alert(nome_completo); 
+
+*/
+
+
+/*
+
+// let é utilizado para referenciar uma variavel apenas no escopo do arquivo, var é uma variavel global
+let nome = "Luis Henrique";
+
+if (nome == "Luis Henrique") {
+    let idade = 90;
+    console.log(idade);
+}
+
+*/
+
+//variavel de valor constante
+
+const nome = {nome: "Luis Henrique", sobrenome: "Santos Pereira"};
+
+console.log(nome);
+
+
