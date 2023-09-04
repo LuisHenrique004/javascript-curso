d2eba147761d32b5b72dc34a22a213482b8b1e3c
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-03 23:20:27 -0300

    Eventos de teclado

---
+++ modulo02/eventos/evento-teclado/script.js
@@ -0,0 +1,8 @@
+function digitou(e) {
+
+    if (e.keyCode == 13 && e.ctrlKey == true) { // Enter + CTRL
+        let texto = document.getElementById("campo").value;
+        console.log(texto);
+    }
+
+};
