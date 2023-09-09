82e3218eb017c4bb82a6554c3cf1077e96a51cb9
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-09 00:22:12 -0300

    Estilos in javascript

--- modulo02/eventos/estilo/script.js
+++ modulo02/eventos/estilo/script.js
@@ -1,8 +1,25 @@
-function digitou(e) {
+function azul() {
+    limpar();
+    document.getElementById("titulo").classList.add('azul');
+}
 
-    if (e.keyCode == 13 && e.ctrlKey == true) { // Enter + CTRL
-        let texto = document.getElementById("campo").value;
-        console.log(texto);
-    }
+function vermelho() {
+    limpar();
+    document.getElementById("titulo").classList.add('vermelho');
+}
 
-};
+function verde() {
+    limpar();
+    document.getElementById("titulo").classList.add('verde');
+}
+
+function limpar() {
+    document.getElementById("titulo").classList.remove('azul');
+    document.getElementById("titulo").classList.remove('vermelho');
+    document.getElementById("titulo").classList.remove('verde');
+}
+
+function mostrarTelefone(elemento) {
+    elemento.style.display = "none";
+    document.getElementById("telefone").style.display = "block";
+}
