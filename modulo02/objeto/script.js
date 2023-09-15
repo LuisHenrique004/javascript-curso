45f0a3b99915b7ae2e115207de3f6cf40d76589b
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-14 22:13:33 -0300

    objetos pt2

--- modulo02/objeto/script.js
+++ modulo02/objeto/script.js
@@ -4,14 +4,30 @@ let carro = {
     nome: 'Fiat',
     modelo: 'Uno',
     peso: '800kg',
+    ligado: false,
     ligar: function() {
+        this.ligado = true;
         console.log("Vrum, Vrum!");
     },
     acelerar: function() {
-        console.log("ririririririri");
+        if (this.ligado == true) {
+            console.log("ririririririri");
+        } else {
+            console.log(this.nome + " " + this.modelo + " " + "Não está ligado!");
+        }
+       
     }
 };
 
 console.log(carro.nome);
 carro.ligar();
 carro.acelerar();
+
+let carrinhos = [
+    {nome: 'Fiat', modelo: 'Palio'},
+    {nome: 'Fiat', modelo: 'Uno'},
+    {nome: 'Toyota', modelo: 'Corolla'},
+    {nome: 'Ferrari', modelo: 'Spider'},
+];
+
+console.log(carrinhos[2].modelo);
