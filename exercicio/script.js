7e90797bc01bf9af8df27184e1bec9c0a0fd3543
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-22 21:36:37 -0300

    exercício em js e aprendendo sobre ponto e virgula

---
+++ exercicio/script.js
@@ -0,0 +1,8 @@
+let carro = "Ferrari";
+console.log(carro);
+
+let preco = 19.99;
+console.log("R$ " + preco);
+
+let cidade = "São Paulo";
+console.log(cidade);
---
+++ modulo01/ponto-virgula/script.js
@@ -0,0 +1,5 @@
+let nome = "Luis Henrique";
+let idade = 22;
+
+console.log(nome);
+console.log(idade);
