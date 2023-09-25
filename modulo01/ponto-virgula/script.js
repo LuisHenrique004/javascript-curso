d2831106c2b472c44411b9b48f6f025efe1e2b50
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-25 04:21:16 -0300

    Condicionais

--- modulo01/ponto-virgula/script.js
+++ modulo01/ponto-virgula/script.js
@@ -3,3 +3,10 @@ let idade = 22;
 
 console.log(nome);
 console.log(idade);
+
+if (idade === 22) {
+    idade += 15;
+    console.log("Você tem 22 anos");
+    console.log("Idade adicionada");
+    console.log(idade);
+}
