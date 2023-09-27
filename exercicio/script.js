ba9952a45b5c8cf4c541f344eb0ef800814cd6c3
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-27 04:26:17 -0300

    exercício 2

--- exercicio/script.js
+++ exercicio/script.js
@@ -6,3 +6,24 @@ console.log("R$ " + preco);
 
 let cidade = "São Paulo";
 console.log(cidade);
+
+
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+let w = "10";
+let z = 10;
+console.log(w === z); 
+
+let a = 10;
+let b = 5;
+console.log(a != b);
+
+let preco_carne = 45.3;
+
+if (preco_carne <= 45.00) {
+    console.log(`O valor da carne por ${preco_carne} está muito barato`);
+} else {
+    console.log(`A carne a ${preco_carne} está muito cara!`);
+}
