936cb28e9e7803f9294863f7a7cdbd44b0ccf8e4
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-27 04:33:00 -0300

    condicional ternaria

--- modulo01/ternario/script.js
+++ modulo01/ternario/script.js
@@ -1,1 +1,6 @@
+let isMember = true;
 
+let shipping = isMember ? 2 : 10;
+
+console.log(isMember ? `Você é membro então paga R$${shipping} de frete` : `Você não é membro então tem que pagar R$${shipping} de frete`);
+console.log(`Frete: ${shipping}`);
