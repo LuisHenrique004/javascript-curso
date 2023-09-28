e0c5722c6e24d4ef8fea9f10738d31680d8ca1ef
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-28 01:46:29 -0300

    exercício sobre funções pt2

--- exercicio/script.js
+++ exercicio/script.js
@@ -30,8 +30,6 @@ if (preco_carne <= 45.00) {
     console.log(`A carne a ${preco_carne} está muito cara!`);
 }
 
-*/
-
 function calcPct(x, y) {
     return ((y / x) * 100);
 }
@@ -40,3 +38,30 @@ let x = 40;
 let y = 10;
 let pct = calcPct(x, y);
 console.log(`${pct}% de ${x} é ${y}`);
+
+*/
+
+function calcularImovel(metragem, quartos) {
+    let m2 = 3000;
+    let preco = 0;
+    switch(quartos) {
+        case 1:
+            preco = metragem * m2;
+            break;
+
+        case 2:
+            preco = metragem * (m2 * 1.2);
+            break;
+
+        case 3:
+            preco = metragem * (m2 * 1.5);
+            break;
+    }
+
+    return preco;
+}
+
+let metragem = 123;
+let quartos = 3;
+let preco = calcularImovel(metragem, quartos);
+console.log(`A casa custa R$ ${preco}`)
