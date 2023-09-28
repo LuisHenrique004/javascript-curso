853a3c318c3b75a6e98a4e47c766c42370d98cad
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-09-28 01:18:21 -0300

    Aprendendo sobre funções

---
+++ modulo02/funcao/script.js
@@ -0,0 +1,43 @@
+// O que é uma função?
+
+/*
+
+function gravidade() {
+    console.log(`A gravidade do planeta é:`);
+    console.log(9.8);
+}
+
+gravidade();
+
+function somar(n1, n2) {
+    let resultado = n1 + n2;
+    console.log(`Resultado: ${resultado}`);
+}
+
+somar(10, 15);
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+}
+
+let completo = nomeCompleto("Luis Henrique", "Santos Pereira")
+console.log(completo);
+
+function maiorDeIdade(idade) {
+    if (idade >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let verificacao = maiorDeIdade(15);
+console.log(verificacao);
+
+if(verificacao) {
+    console.log("é maior de idade");
+} else {
+    console.log("é menor de idade");
+}
+
+*/
