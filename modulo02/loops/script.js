017ad53959a9c0fa6aa6b670499011fb0d31b83b
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-10-04 02:59:10 -0300

    loop for

---
+++ modulo02/loops/script.js
@@ -0,0 +1,21 @@
+let cores = [
+    {nome: 'preto', qt: 10},
+    {nome: 'azul', qt:15},
+    {nome: 'vermelho', qt: 15}
+];
+
+/*
+for(let n = 0; n < cores.length; n++) {
+    console.log(`Cor: ${cores[n]}`);
+}
+
+
+for (let i in cores) {
+    console.log(`Cor: ${cores[i]}`);
+}
+
+*/
+
+for (let cor of cores) {
+    console.log(`Cor: ${cor.nome} + Quantidade: ${cor.qt}`);
+}
