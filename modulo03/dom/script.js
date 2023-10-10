a59025932601a7345ad10aae9930d6c17d525eed
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-10-10 01:40:18 -0300

    append and prepend

--- modulo03/dom/script.js
+++ modulo03/dom/script.js
@@ -2,8 +2,27 @@ function clicou() {
     const teste = document.querySelector("#teste");
     const ul = teste.querySelector("ul");
     
-    
-    //outerHTML pega o conteudo de fora das tag
-    console.log(ul.outerHTML);
-    ul.outerHTML = "<strong>123</strong>"
+/*
+
+//outerHTML pega o conteudo de fora das tag
+console.log(ul.outerHTML);
+ul.outerHTML = "<strong>123</strong>"
+
+//adiciona mais conteudo ao elemento, não muda o endereço de memoria
+ul.children[0].append("(alterado)");
+
+
+//essa forma é melhor em termos de processamento, por que não recria o elemento na memoria.
+let newLi = document.createElement("li");
+newLi.innerText = "Item adicionado";
+
+ul.append(newLi);
+
+let newLi = document.createElement("li");
+newLi.innerText = "Item adicionado";
+//adiciona no começo!
+ul.prepend(newLi);
+
+*/
+
 }
